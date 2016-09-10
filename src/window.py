73e547a31d72bc51c8a9fde92659f74997399782
Siver Salih <siversalih@outook.com>
2016-09-10 00:10:02 -0400

    Small function tweak

--- src/window.py
+++ src/window.py
@@ -21,51 +21,51 @@ class Window:
     config_filename = None
     driver = None
     drivername = ""
-    position = ()
-    size = ()
-    zoom = 0
-    scroll = ()
+    positionWin = ()
+    sizeWin = ()
+    zoomWin = 0
+    scrollWin = ()
 
     def __init__(self,driver, config = 0):
         self.driver = driver
         self.config_filename = "config.json"
         self.drivername = "chromedriver"
-        self.position = (100,50)
-        self.size = (720,480)
-        self.zoom = 70
-        self.scroll = (0,0)
+        self.positionWin = (100,50)
+        self.sizeWin = (720,480)
+        self.zoomWin = 70
+        self.scrollWin = (0,0)
 
         if config:
             self.config_filename = config
         with open(self.config_filename) as jsonFile:
             config = json.load(jsonFile)
 
-        position = config.get('position')
-        x = position[0]
-        y = position[1]
-        position = self.validatePosition(position)
-        if position:
-            self.position = position
+        positionWin = config.get('position')
+        x = positionWin[0]
+        y = positionWin[1]
+        positionWin = self.validatePosition(positionWin)
+        if positionWin:
+            self.positionWin = positionWin
 
-        size = config.get('size')
-        w = size[0]
-        h = size[1]
-        size = self.validateSize((w,h))
-        if size:
-            self.size = size
+        sizeWin = config.get('size')
+        w = sizeWin[0]
+        h = sizeWin[1]
+        sizeWin = self.validateSize((w,h))
+        if sizeWin:
+            self.sizeWin = sizeWin
 
-        zoom = config.get('zoom')
-        if self.validateZoom(zoom):
-            self.zoom = zoom
+        zoomWin = config.get('zoom')
+        if self.validateZoom(zoomWin):
+            self.zoomWin = zoomWin
 
-        scroll = config.get('scroll')
-        if self.validateScroll(scroll):
-            self.scroll = scroll
+        scrollWin = config.get('scroll')
+        if self.validateScroll(scrollWin):
+            self.scrollWin = scrollWin
 
-        self.sizeWindow(self.size)
-        self.positionWindow(self.position)
-        self.zoomWindow(self.zoom)
-        self.scrollWindow(self.scroll)
+        self.size(self.sizeWin)
+        self.position(self.positionWin)
+        self.zoom(self.zoomWin)
+        self.scrol(self.scrollWin)
 
     def dealloc(self):
         if self.config_filename:
@@ -74,16 +74,16 @@ class Window:
             del self.driver
         if self.drivername:
             del self.drivername
-        if self.position:
-            del self.position
-        if self.size:
-            del self.size
-        if self.zoom:
-            del self.zoom
-        if self.scroll:
-            del self.scroll
+        if self.positionWin:
+            del self.positionWin
+        if self.sizeWin:
+            del self.sizeWin
+        if self.zoomWin:
+            del self.zoomWin
+        if self.scrollWin:
+            del self.scrollWin
 
-    def positionWindow(self, windowPosition):
+    def position(self, windowPosition):
         if not isinstance(windowPosition,tuple):
             print "Invalid Window Position Format {}".format(windowPosition)
             return 1
@@ -91,14 +91,14 @@ class Window:
         if windowPosition == 0:
             print "Invalid Window Position Format {}".format(windowPosition)
             return 1
-        self.position = windowPosition
-        self.driver.set_window_position(self.position[0],self.position[1])
+        self.positionWin = windowPosition
+        self.driver.set_window_position(self.positionWin[0],self.positionWin[1])
         actualPosition = self.driver.get_window_position()
-        self.position = [actualPosition.get('x'),actualPosition.get('y')]
-        print("Window Position \t X: {} \t Y: {}".format(self.position[0],self.position[1]))
+        self.positionWin = [actualPosition.get('x'),actualPosition.get('y')]
+        print("Window Position \t X: {} \t Y: {}".format(self.positionWin[0],self.positionWin[1]))
         return 0
 
-    def sizeWindow(self, windowSize):
+    def size(self, windowSize):
         if not isinstance(windowSize,tuple):
             print "Invalid Window Size Format {}".format(windowSize)
             return 1
@@ -106,41 +106,41 @@ class Window:
         if windowSize == 0:
             print "Invalid Window Size Format {}".format(windowSize)
             return 1
-        self.size = windowSize
-        self.driver.set_window_size(self.size[0], self.size[1])
+        self.sizeWin = windowSize
+        self.driver.set_window_size(self.sizeWin[0], self.sizeWin[1])
         actualSize = self.driver.get_window_size()
-        self.size = [actualSize.get('width'),actualSize.get('height')]
-        print("Window Size \t\t W: {} \t H: {}".format(self.size[0],self.size[1]))
+        self.sizeWin = [actualSize.get('width'),actualSize.get('height')]
+        print("Window Size \t\t W: {} \t H: {}".format(self.sizeWin[0],self.sizeWin[1]))
         return 0
 
-    def zoomWindow(self, percent):
+    def zoom(self, percent):
         if not isinstance(percent,int):
             print "Invalid Value format for Percent {}".format(percent)
             return 1
         if self.validateZoom(percent) == 0:
             print "Zoom {} not in range (10-100)".format(percent)
             return 1
-        self.zoom = percent
+        self.zoomWin = percent
         self.driver.execute_script("document.body.style.zoom='{}%'".format(self.zoom))
         print "Window Zoom: \t\t {}".format(self.zoom)
         return 0
 
     def zoomIn(self):
-        if self.zoom + 10 > 100:
-            print "Can't Zoom In to {}%".format(self.zoom+10)
+        if self.zoomWin + 10 > 100:
+            print "Can't Zoom In to {}%".format(self.zoomWin+10)
             return 1
-        print "Zooming In: {} -> {}".format(self.zoom,self.zoom+10)
-        self.zoom = self.zoom + 10
-        self.zoomWindow(self.zoom)
+        print "Zooming In: {} -> {}".format(self.zoomWin,self.zoomWin+10)
+        self.zoomWin = self.zoomWin + 10
+        self.zoom(self.zoomWin)
         return 0
 
     def zoomOut(self):
-        if self.zoom - 10 < 1:
-            print "Can't Zoom Out to {}%".format(self.zoom-10)
+        if self.zoomWin - 10 < 1:
+            print "Can't Zoom Out to {}%".format(self.zoomWin-10)
             return 1
-        print "Zooming Out: {} -> {}".format(self.zoom,self.zoom-10)
-        self.zoom = self.zoom - 10
-        self.zoomWindow(self.zoom)
+        print "Zooming Out: {} -> {}".format(self.zoomWin,self.zoomWin-10)
+        self.zoomWin = self.zoomWin - 10
+        self.zoom(self.zoomWin)
         return 0
 
     def scrollToElement(self, element):
@@ -153,86 +153,87 @@ class Window:
         if isinstance(location_dic,dict):
             x = location_dic.get('x')
             y = location_dic.get('y')
-            self.scroll = (int(x),int(y))
+            self.scrollWin = (int(x),int(y))
             print "Scrolling to point {}".format(element.location)
             time.sleep(1)
             return 0
         else:
             return 1
 
-    def scrollWindow(self,scroll):
-        if not isinstance(scroll,tuple):
-            print "Invalid Scroll Format {}".format(scroll)
+    def scrol(self,scrollWin):
+        if not isinstance(scrollWin,tuple):
+            print "Invalid Scroll Format {}".format(scrollWin)
             return 1
-        if self.validateScroll(scroll) == 0:
-            print "Can't scroll to ({},{})".format(scroll[0],scroll[1])
+        if self.validateScroll(scrollWin) == 0:
+            print "Can't scroll to ({},{})".format(scrollWin[0],scrollWin[1])
             return 1
         else:
-            self.scroll = scroll
-            x = scroll[0]
-            y = scroll[1]
+            self.scrollWin = scrollWin
+            x = scrollWin[0]
+            y = scrollWin[1]
             self.driver.execute_script("window.scrollTo({}, {})".format(x,y))
-            print "Window Scroll: \t\t {}".format(self.scroll)
+            print "Window Scroll: \t\t {}".format(self.scrollWin)
         return 0
 
     def scrollDown(self):
         scrollHeight = self.driver.execute_script("return document.body.scrollHeight")
         step = (scrollHeight / 10)
-        x = self.scroll[0]
-        y = self.scroll[1]
+        x = self.scrollWin[0]
+        y = self.scrollWin[1]
         if (y + step) > scrollHeight:
             print "Can't Scroll Down to {}".format(y + step)
             return 1
         print "Scrolling Down: {} -> {}".format(y,y+step)
-        self.scroll = (x,y+step)
-        self.scrollWindow(self.scroll)
+        self.scrollWin = (x,y+step)
+        self.scrol(self.scrollWin)
+
         return 0
 
     def scrollUp(self):
         scrollHeight = self.driver.execute_script("return document.body.scrollHeight")
         step = (scrollHeight / 10)
-        x = self.scroll[0]
-        y = self.scroll[1]
+        x = self.scrollWin[0]
+        y = self.scrollWin[1]
         if (y - step) < 0:
             print "Can't Scroll Up to {}".format(y - step)
             return 1
         print "Scrolling Up: {} -> {}".format(y,y-step)
-        self.scroll = (x,y-step)
-        self.scrollWindow(self.scroll)
+        self.scrollWin = (x,y-step)
+        self.scrol(self.scrollWin)
         return 0
 
     def scrollRight(self):
         scrollWidth = self.driver.execute_script("return document.body.scrollWidth")
         step = (scrollWidth / 10)
-        x = self.scroll[0]
-        y = self.scroll[1]
+        x = self.scrollWin[0]
+        y = self.scrollWin[1]
         if (x + step) > scrollWidth:
             print "Can't Scroll Right to {}".format(x + step)
             return 1
         print "Scrolling Right: {} -> {}".format(x,x+step)
-        self.scroll = (x+step,y)
-        self.scrollWindow(self.scroll)
+        self.scrollWin = (x+step,y)
+        self.scrol(self.scrollWin)
         return 0
 
     def scrollLeft(self):
         scrollWidth = self.driver.execute_script("return document.body.scrollWidth")
         step = (scrollWidth / 10)
-        x = self.scroll[0]
-        y = self.scroll[1]
+        x = self.scrollWin[0]
+        y = self.scrollWin[1]
         if (x - step) < 0:
             print "Can't Scroll Left to {}".format(x - step)
             return 1
         print "Scrolling Left: {} -> {}".format(x,x-step)
-        self.scroll = (x-step,y)
-        self.scrollWindow(self.scroll)
+        self.scrollWin = (x-step,y)
+        self.scrol(self.scrollWin)
         return 0
 
-    def validateSize(self, size):
-        if not size and not len(size) == 2:
-            print ("Invalid Size Format {}".format(size))
+    def validateSize(self, sizeWin):
+        if not sizeWin and not len(sizeWin) == 2:
+            print ("Invalid Size Format {}".format(sizeWin))
             return 0
-        w = size[0]
-        h = size[1]
+        w = sizeWin[0]
+        h = sizeWin[1]
         if (w < 400):
             print "Min: {} Passed: {}".format(400,w)
             w = 400
@@ -245,8 +246,8 @@ class Window:
         elif (h > 1000):
             print "Max: {} Passed: {}".format(1000,h)
             h = 1000
-        size = (w,h)
-        return size
+        sizeWin = (w,h)
+        return sizeWin
 
     def validateZoom(self, percent):
         if not isinstance(percent,int):
@@ -260,12 +261,12 @@ class Window:
             return 0
         return 1
 
-    def validateScroll(self, scroll):
-        if not isinstance(scroll,tuple):
-            print "Invalid Scroll Format {}".format(scroll)
+    def validateScroll(self, scrollWin):
+        if not isinstance(scrollWin,tuple):
+            print "Invalid Scroll Format {}".format(scrollWin)
             return 0
-        x = int(scroll[0])
-        y = int(scroll[1])
+        x = int(scrollWin[0])
+        y = int(scrollWin[1])
         scrollHeight = 100
         scrollWidth = 100
         if self.driver:
@@ -283,14 +284,15 @@ class Window:
         if (y > scrollHeight):
             print "Y Max: {} Passed: {}".format(scrollHeight,y)
             return 0
-        return 1
+        scrollWin = (x,y)
+        return scrollWin
 
-    def validatePosition(self, position):
-        if not position and not len(position) == 2:
-            print ("Invalid Position Format {}".format(position))
+    def validatePosition(self, positionWin):
+        if not positionWin and not len(positionWin) == 2:
+            print ("Invalid Position Format {}".format(positionWin))
             return 0
-        x = position[0]
-        y = position[1]
+        x = positionWin[0]
+        y = positionWin[1]
         if (x < 0):
             print "Min: {} Passed: {}".format(0,x)
             x = 0
@@ -303,6 +305,6 @@ class Window:
         elif (y > 1000):
             print "Max: {} Passed: {}".format(1000,y)
             y = 1000
-        position = (x,y)
-        return position
+        positionWin = (x,y)
+        return positionWin
 

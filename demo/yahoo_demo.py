13b14546c87cd824b9dd4fa8bad1d331b902afa6
Siver Salih <siversalih@outook.com>
2016-09-14 07:31:23 -0400

    New demo added.

--- demo/yahoo_demo.py
+++ demo/yahoo_demo.py
@@ -1,21 +1,24 @@
 from time import sleep
 import os
+import sys
 
 par_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 if(not os.path.exists(par_dir)):
     print("Path {} does not exist".format(par_dir))
     exit(1)
 os.chdir(par_dir)
+sys.path.insert(0, par_dir)
 
 try:
     from pyChrome import PyChrome
 except ImportError:
-    print "pyChrome.py is missing...Exiting program."
+    print "pyChrome.py is missing from {}\nTry copy {} to pyChrome.py directory and run it from there.".format(os.getcwd(),os.path.basename(__file__))
     exit(1)
+
 
 print "\n"
 print "***** Welcome to pyChrome Demo *****"
-print "\nThis demo shows how one could leverage pyChrome engine to autonomously perform the following tasks"
+print "\nThis demo shows how one could leverage pyChrome engine to perform the following tasks autonomously"
 
 print "1. Open the URL www.yahoo.com \n" \
       "2. Find the most trending topic on Yahoo.com \n" \
@@ -49,6 +52,6 @@ browser.selectElement(elements[1])
 browser.clickLink()
 
 print "\n*****Demo Complete*****"
-raw_input('\nPress Enter to Quit\n>')
+raw_input('\nPress Enter to Quit\n> ')
 
 browser.quit()
